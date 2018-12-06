7d26b3434d7298f0933c48df01c18c01517e0dc2
Julian Leuze <leuze.julian@gmail.com>
2018-12-06 21:37:45 +0100

    Init of leap module can be switched of by cmd. Externalized leap parameters. Not emitting more than 60 Frames per second. Swipe left will now load previous model.

--- leap.js
+++ leap.js
@@ -1,4 +1,13 @@
-Leap = require('leapjs');
+const args = process.argv;
+console.log(`Starting with args: ${args}`);
+if(args.length < 3 || args[2] !== 'noleap') {
+  console.log('Initializing leap')
+  initLeap();
+} else {
+  console.log('Gabriel mode... initializing leap skipped')
+}
+var Leap;
+var globalFrame;
 var express = require('express')
 var app = express();
 var http = require('http').Server(app);
@@ -9,12 +18,12 @@ app.use(express.static('js'))
 app.use(express.static('models'))
 
 lastFrame = null;
-previous = 0;
-xDelta = 0;
 isConnected = false;
 gSocket = null
 models = fs.readdirSync('./models/');
+settingsFile = fs.readFileSync('./parameters.json')
 modelIndex = 0;
+settings = JSON.parse(settingsFile);
 console.log(models)
 
 models.forEach(file => {
@@ -31,128 +40,119 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-var controller = new Leap.Controller({ enableGestures: true});
-controller.connect();
-console.log(controller.config);
-
-function getMovementDirection(frame) {
-		var hand = frame.hands[0];
-		var velocityX = hand.palmVelocity[0];
-		console.log(velocityX);
-		if(Math.abs(velocityX) > 5) {
-
-			if(velocityX > 5.0) {
-				return 1;
-			} else if(velocityX < -5.0)
-				return -1;
-		}
-		return 0;
+function handleTranslation(frame) {
+  gSocket.emit('translateModel', { "x" : frame.hands[0].palmVelocity[0],
+              "y" : frame.hands[0].palmVelocity[1],
+              "z": frame.hands[0].palmVelocity[2]}
+  )
+  xAbs = Math.abs(frame.hands[0].palmVelocity[0]);
+  yAbs = Math.abs(frame.hands[0].palmVelocity[1]);
+  // if(xAbs > 15) {
+    console.log(`x: ${xAbs}`);
+  // }
+  // if(yAbs > 15) {
+    console.log(`y: ${yAbs}`);
+  // }
 }
 
-function getDelta(frame) {
-	var hand = frame.hands[0];
-	xDelta = hand.direction[0] - previous;
-	previous = hand.direction[0];
-	return xDelta;
-}
-
-function getPosition(frame) {
-	var hand = frame.hands[0];
-	return hand.palmPosition[0];
-}
-
-function isGrabbing(frame) {
-	var hand = frame.hands[0];
-	console.log(hand.grabStrength);
-}
-globalFrame = null;
-controller.on('frame', onFrame);
+var tempFrame;
 function onFrame(frame)
 {
 	globalFrame = frame;
-	if(frame.hands && frame.hands.length !== 0) {
-		//console.log(getMovementDirection(frame))
-		//isGrabbing(frame);
+	if(frame.valid && frame.hands && frame.hands.length !== 0 && (!tempFrame || (frame.timestamp - tempFrame.timestamp > 16666))) {
+    tempFrame = frame;
 		if(isConnected) {
-			//console.log(frame.hands[0].palmVelocity[0],frame.hands[0].palmVelocity[1], frame.hands[0].grabStrength)
-			if(frame.hands[0].grabStrength > 0.6) {
-				gSocket.emit('translateModel', { "x" : frame.hands[0].palmVelocity[0],
-										"y" : frame.hands[0].palmVelocity[1],
-										"z": frame.hands[0].palmVelocity[2]}
-				)
-				xAbs = Math.abs(frame.hands[0].palmVelocity[0]);
-				yAbs = Math.abs(frame.hands[0].palmVelocity[1]);
-				if(xAbs > 15) {
-					console.log(`x: ${xAbs}`);
-				}
-				if(yAbs > 15) {
-					console.log(`y: ${yAbs}`);
-				}
+       //NOTE: Externalized Value: grabStrength
+			if(frame.hands[0].grabStrength >= settings.grabStrength) {
+        handleTranslation(frame);
 			}
-			/*console.log({ "velocity" : frame.hands[0].palmVelocity[0],
-								   "grabStrength" : frame.hands[0].grabStrength,
-									"velocityY": frame.hands[0].palmVelocity[1]})*/
 		}
-		//console.log(getDelta(frame));
-		//console.log(getPosition(frame));
 	}
 }
 
 function isWholeSwipe(start, current) {
-	return start && (current[0] - start[0]) > 100;
+  if(start) {
+    console.log(`distance: ${current[0] - start[0]}`)
+    var maxVal = Math.max(current[0], start[0]);
+    var minVal = Math.min(current[0], start[0]);
+    return (maxVal - minVal) > settings.gesture.swipe.minLength; //NOTE: externalized value: swipe.minLength
+  }
+  return false;
 }
 
-controller.on('gesture', function(gesture) {
-	if(gesture.type == 'circle') {
-		console.log(gesture.state)
-		if(gesture.state == 'start') {
-			console.log('START')
-		} else if(gesture.state == 'update') {
-					pointableIds = gesture.pointableIds;
-					pointableIds.forEach(function(pointableId) {
-					var pointable = globalFrame.pointable(pointableId);
-					console.log(`center: ${gesture.center}, radius: ${gesture.radius}`)
-					console.log(pointable.stabilizedTipPosition)
-					console.log(`input ${(pointable.tipPosition[0] - gesture.center[0]) / gesture.radius}`)
-					deg = Math.acos((pointable.tipPosition[0] - gesture.center[0]) / gesture.radius)
-					console.log(deg);
-					gSocket.emit('rotateModel', deg * 100);
-					deg2 = Math.asin((pointable.tipPosition[1] - gesture.center[1]) / gesture.radius)
-					console.log(deg2)
-				});
-			} else if(gesture.state == 'stop') {
-				console.log('END')
-			}
+function direction(gesture) {
+  var pointableID = gesture.pointableIds[0];
+  var direction = globalFrame.pointable(pointableID).direction;
+  var dotProduct = Leap.vec3.dot(direction, gesture.normal);
+
+  return (dotProduct  >  0);
+}
+
+clockwise = true;
+function handleCircle(gesture) {
+  console.log(gesture.state)
+  if(gesture.state == 'start') {
+    clockwise = direction(gesture);
+    console.log('START Circling, Clockwise:', clockwise)
+  } else if(gesture.state == 'update') {
+        pointableIds = gesture.pointableIds;
+        pointableIds.forEach(function(pointableId) {
+        var pointable = globalFrame.pointable(pointableId);
+        console.log(`center: ${gesture.center}, radius: ${gesture.radius}`)
+        console.log(pointable.stabilizedTipPosition)
+        console.log(`input ${(pointable.tipPosition[0] - gesture.center[0]) / gesture.radius}`)
+        deg = Math.acos((pointable.tipPosition[0] - gesture.center[0]) / gesture.radius)
+        console.log(deg);
+        if(isConnected) {
+          gSocket.emit('rotateModel', deg * settings.gesture.circle.multiplier); //NOTE: externalized value: circle.multiplier
+        }
+      });
+    } else if(gesture.state == 'stop') {
+      console.log('END')
+    }
+}
+
+function handleSwipe(gesture) {
+  if(gesture.state == 'start') {
+    console.log('START SWIPE')
+    start = gesture.startPosition;
+  } else if(gesture.state == 'stop') {
+    console.log('STOP SWIPE', start, gesture.position, `speed: ${gesture.speed}`)
+    if(isWholeSwipe(start, gesture.position)) {
+      if(gesture.direction[0] > 0) {
+        console.log('Swipe to right')
+        modelIndex = (modelIndex + 1) % models.length;
+        gSocket.emit('changeModel', models[modelIndex])
+      } else {
+        console.log('swipe to left')
+        modelIndex--;
+        if(modelIndex < 0) {
+          modelIndex = models.length -1;
+        }
+        gSocket.emit('changeModel', models[modelIndex])
+      }
+      console.log(`changing Model to ${models[modelIndex]}`)
+      console.log('WHOLE SWIPE', start, gesture.position)
+    }
+    start = null;
+  }
+}
+
+function handleGesture(gesture) {
+  if(gesture.type == 'circle') {
+      handleCircle(gesture);
 		} else if(gesture.type == 'swipe') {
-			if(gesture.state == 'start') {
-				console.log('START SWIPE')
-				start = gesture.startPosition;
-			} else if(gesture.state == 'stop') {
-				console.log('STOP SWIPE', start, gesture.position)
-				if(isWholeSwipe(start, gesture.position)) {
-					modelIndex = (modelIndex + 1) % models.length;
-					console.log(`changing Model to ${models[modelIndex]}`)
-					gSocket.emit('changeModel', models[modelIndex])
-					console.log('WHOLE SWIPE', start, gesture.position)
-				}
-				start = null;
-			}
-			if(gesture.direction[0] > 0) {
-                  swipeDirection = "right";
-									if(gesture.state == 'update') {
-										// if(isWholeSwipe(start, gesture.position)) {
-										// 	// modelIndex = (modelIndex + 1) % models.length;
-										// 	// console.log(`changing Model to ${models[modelIndex]}`)
-										// 	// gSocket.emit('changeModel', models[modelIndex])
-										// 	console.log('WHOLE SWIPE', start, gesture.position)
-										// }
-										console.log('UPDATE SWIPE')
-									}
-              } else {
-                  swipeDirection = "left";
-              }
+      handleSwipe(gesture);
 		}
-});
+}
+
+function initLeap() {
+  Leap = require('leapjs');
+  controller = new Leap.Controller({enableGestures: true});
+  controller.connect();
+  controller.on('frame', onFrame);
+  controller.on('gesture', handleGesture);
+}
 
 
 http.listen(3000, function() {
